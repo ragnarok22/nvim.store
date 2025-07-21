e0682ee82dcced7916d1f72c6a0aca178f1ec27a
Reinier Hernández <8838803+ragnarok22@users.noreply.github.com>
2025-07-21 13:36:45 -0300

    feat: add installation modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import Help from "@/components/help";
+import InstallModal from "@/components/install-modal";
 import RepositoryWrapper from "@/components/repository-wrapper";
 import { retrievePlugins } from "@/lib/api";
 
@@ -12,6 +13,7 @@ export default async function Home() {
       <RepositoryWrapper repositories={repositories} />
 
       <Help />
+      <InstallModal />
     </div>
   );
 }

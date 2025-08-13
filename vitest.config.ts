c4156790d0a0940825f3651680deda0cc4bc59a3
Reinier Hernández <8838803+ragnarok22@users.noreply.github.com>
2025-08-13 10:00:33 -0300

    test: add component tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   test: {
     environment: "node",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
+    },
+  },
+  esbuild: {
+    jsx: "automatic",
   },
 });

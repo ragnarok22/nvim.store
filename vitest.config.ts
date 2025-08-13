a9ef0e2090f4cf9fb2be867850632c91da33bbaa
Reinier Hernández <8838803+ragnarok22@users.noreply.github.com>
2025-08-13 10:06:04 -0300

    test: enable coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,11 @@ import path from "path";
 export default defineConfig({
   test: {
     environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "coverage",
+    },
   },
   resolve: {
     alias: {
